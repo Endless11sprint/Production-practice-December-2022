b58886bae474d978f1d1c786f683a0b92eae009c
Endless11sprint <94888543+Endless11sprint@users.noreply.github.com>
2022-12-19 16:57:51 +0300

    Add files via upload
    Входе разработки возникла проблема, из-за того, что данный пример (https://learn.microsoft.com/en-us/windows/apps/design/input/convert-ink-to-text) на .Net Core, а  System.Speech.Synthesis работает на  .Net Framework. И есть два решения или .Net Core реализовать другой способ озвучки или  работать на .Net Framework (для чего и нужен этот commit).

---
+++ InkRecognition/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using Microsoft.Ink;
+using System.IO;
+using System;
+
+namespace InkRecognition
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                theInkCanvas.Strokes.Save(ms);
+                var myInkCollector = new InkCollector();
+                var ink = new Ink();
+                ink.Load(ms.ToArray());
+                using (RecognizerContext context = new RecognizerContext())
+                {
+                    if (ink.Strokes.Count > 0)
+                    {
+                        context.Strokes = ink.Strokes;
+                        RecognitionStatus status;
+                        var result = context.Recognize(out status);
+                        if (status == RecognitionStatus.NoError)
+                            textBox1.Text = result.TopString;
+                        else
+                        {
+                            MessageBox.Show("Recognition failed");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No stroke detected");
+                    }
+                }
+            }
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            theInkCanvas.Strokes.Clear();
+        }
+    }
+}
