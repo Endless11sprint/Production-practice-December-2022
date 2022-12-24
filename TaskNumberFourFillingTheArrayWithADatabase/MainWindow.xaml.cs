c9feb88120881dbc1ba03a6e3bf015b86c396f38
Endless11sprint <94888543+Endless11sprint@users.noreply.github.com>
2022-12-24 21:21:05 +0300

    Создание бинарного массива для бд
    Создание бинарного массива звукового файла рассширения.wav, для последующей записи его в бд.

---
+++ TaskNumberFourFillingTheArrayWithADatabase/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.Windows;
+using Microsoft.Ink;
+using System.IO;
+using System.Speech.Synthesis; 
+using System;
+using System.Data.Entity;
+
+namespace TaskNumberFourFillingTheArrayWithADatabase
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            VoicedWord = GetAllEnglishWord();
+            DataContext = VoicedWord;
+        }
+        string VoicedWord;
+
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                theInkCanvas.Strokes.Save(ms);
+                var myInkCollector = new InkCollector();
+                var ink = new Ink();
+                ink.Load(ms.ToArray());
+                using (RecognizerContext context = new RecognizerContext())
+                {
+                    if (ink.Strokes.Count > 0)
+                    {
+                        context.Strokes = ink.Strokes;
+                        RecognitionStatus status;
+                        var result = context.Recognize(out status);
+                        if (status == RecognitionStatus.NoError)
+                        {
+                            textBox1.Text = result.TopString;
+                            if (result.TopString == VoicedWord)
+                            {
+                                MessageBox.Show("Вы правильно написали озвученное слово!");
+                                VoicedWord = GetAllEnglishWord();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Вы НЕ правильно написали озвученное слово!");
+                                VoicedWord = GetAllEnglishWord();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Распознавание не удалось");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Строка не обнаружена");
+                    }
+                }
+            }
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            theInkCanvas.Strokes.Clear();
+        }
+        public int CountElementsRowWord()
+        {
+            EducationalPracticeEntities context = new EducationalPracticeEntities();
+            context.EnglishWord.Load();
+            int count = context.EnglishWord.Local.Count;
+            return count;
+        }
+
+        public string GetAllEnglishWord()
+        {
+            EducationalPracticeEntities context = new EducationalPracticeEntities();
+            int numberOfEntriesInTheWordsColumn = CountElementsRowWord();
+            string[] arrWordBD = new string[numberOfEntriesInTheWordsColumn];
+            int count = 0;
+            foreach (EnglishWord englishWord in context.EnglishWord)
+            {
+                arrWordBD[count] = englishWord.Word.ToString();
+                count++;
+            }
+            Random random = new Random();
+            int value = random.Next(0, arrWordBD.Length);
+            return arrWordBD[value];
+        }
+        public void VoiceActingOfTheWord(string theSpokenWord)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            //Озвучка
+            synth.Speak(theSpokenWord);
+        }
+
+        private void btnVoiceOver_Click(object sender, RoutedEventArgs e)
+        {
+            VoiceActingOfTheWord(VoicedWord);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            VoiceActingOfAlreadyAddedWords window1= new VoiceActingOfAlreadyAddedWords();
+            window1.Show();
+        }
+            
+    }
+}
---
+++ TaskNumberFourFillingTheArrayWithADatabase/VoiceActingOfAlreadyAddedWords.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using NAudio.Wave;
+namespace TaskNumberFourFillingTheArrayWithADatabase
+{
+    public partial class VoiceActingOfAlreadyAddedWords : Window
+    {
+        public VoiceActingOfAlreadyAddedWords()
+        {
+            InitializeComponent();
+            ComboSelectWord.ItemsSource = AddingWordsToTheComboboxThatHaveNotYetBeenVoiced();
+            
+        }
+        WaveIn waveIn;
+        WaveFileWriter waveWriter;
+        EducationalPracticeEntities context = new EducationalPracticeEntities();
+        //List<byte> audioBytes = new List<byte>(new byte[] { });
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (!Dispatcher.CheckAccess())
+            {
+                Dispatcher.BeginInvoke(new EventHandler<WaveInEventArgs>(WaveIn_DataAvailable), sender, e);
+            }
+            else
+            {
+                waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            }
+        }
+        void WaveIn_RecordingStopped(object sender, EventArgs e)
+        {
+            if (!Dispatcher.CheckAccess())
+            {
+                Dispatcher.BeginInvoke(new EventHandler(WaveIn_RecordingStopped), sender, e);
+            }
+            else
+            {
+                waveIn.Dispose();
+                waveIn = null;
+                waveWriter.Close();
+                waveWriter = null;
+            }
+        }
+        private IEnumerable<EnglishWord> AddingWordsToTheComboboxThatHaveNotYetBeenVoiced()
+        {
+           
+            context.EnglishWord.Load();
+            var allWordsInEnglish = context.EnglishWord.ToList();
+            var unwrittenWords = from word in allWordsInEnglish
+                                 where word.idPathsToVoiceFilesThatWereCreatedByTheUserEnglishWord_fk_== null
+                                 select word;
+            return unwrittenWords.ToList();
+        }
+        public void StartReconding_Click(object sender, RoutedEventArgs e)
+        {
+            waveIn = new WaveIn();
+            //Дефолтное устройство для записи (если оно имеется)
+            waveIn.DeviceNumber = 0;
+            //Прикрепляем к событию DataAvailable обработчик, возникающий при наличии записываемых данных
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            //Прикрепляем обработчик завершения записи
+            waveIn.RecordingStopped += WaveIn_RecordingStopped;
+            //Формат wav-файла - принимает параметры - частоту дискретизации и количество каналов(здесь mono)
+            waveIn.WaveFormat = new WaveFormat(22050, 1);
+            waveWriter = new WaveFileWriter("intermediateRecordFileInBinary.wav", waveIn.WaveFormat);
+            //Начало записи
+            waveIn.StartRecording();
+        }
+        private void StopRecording_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveIn != null)
+            {
+                waveIn.StopRecording();
+                MessageBox.Show("Запись окончена");
+                //var bytes = File.ReadAllBytes("intermediateRecordFileInBinary.wav");
+            }
+        }
+        private void intermediateRecordFileInBinaryDelet()
+        {
+            FileInfo fileInf = new FileInfo("intermediateRecordFileInBinary.wav");
+            fileInf.Delete();
+        }
+
+        private void PlayTheRecording_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var bytes = File.ReadAllBytes("intermediateRecordFileInBinary.wav");
+
+                using (Stream s = new MemoryStream(bytes.ToArray()))
+                {
+                    System.Media.SoundPlayer myPlayer = new System.Media.SoundPlayer(s);
+                    myPlayer.Play();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Файла не существует или его формат не .wav");
+            }
+        }
+
+        private void SaveRecording_Click(object sender, RoutedEventArgs e)
+        {
+            PathsToVoiceFilesThatWereCreatedByTheUser pathsToVoiceFilesThatWereCreatedByTheUser = new PathsToVoiceFilesThatWereCreatedByTheUser();
+            var bytes = File.ReadAllBytes("intermediateRecordFileInBinary.wav");
+            pathsToVoiceFilesThatWereCreatedByTheUser.Path = bytes.ToString();
+            
+            context.EnglishWord.Load();
+            
+            intermediateRecordFileInBinaryDelet();
+        }
+    }
+}
